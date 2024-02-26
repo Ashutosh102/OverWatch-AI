60afd428275763756d90c974605387b693bef188
Ashutosh Mohanty <ashutoshmohanty3815@gmail.com>
2024-02-27 01:16:33 +0530

    Add files via upload

---
+++ components/social-links.tsx
@@ -0,0 +1,21 @@
+'use client'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+const SocialMediaLinks = () => {
+    return (
+        <div className='flex flex-row gap-4'>
+            <a href="https://github.com/Ashutosh102" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faGithub} size="2x" />
+            </a>
+            <a href="https://www.instagram.com/dev_ashu_102/" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faInstagram} size="2x" />
+            </a>
+            <a href="https://www.linkedin.com/in/devashu/" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faLinkedin} size="2x" />
+            </a>
+        </div>
+    );
+};
+
+export default SocialMediaLinks;
